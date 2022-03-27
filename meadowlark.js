7200eb7163452175bfc874c8dc754a2a77c0498b
Muniox <muniox20@gmail.com>
2022-03-27 23:03:06 +0200

    Initial projectfiles str 50
    Tworzenie aplikacji internetowych z użyciem Node i Express
    
    1. Problem z nową wersją express handlebars
    2. rozwiązanie problemu z znikającym <!DOCTYPE html> poprzez helper handlebars

---
+++ meadowlark.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { create } = require("express-handlebars");
+
+const app = express();
+
+//konfiguracja silnika widokow Handlebars
+const hbs = create({
+  helpers: {
+    doctype() {
+      return "<!DOCTYPE html>";
+    },
+  },
+});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+//konfiguracja portu na którym nasłuchuje serwer
+const port = process.env.PORT || 3000;
+
+//trasowanie
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+//app.use z bledami serwera zawsze ponizej trasowania!
+//Niestandardowa strona 404
+app.use((req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+//Niestandardowa strona 500
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500);
+  res.render("500");
+});
+
+//dodanie plików statycznych
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+  console.log(`Express został uruchomiony pod adresem http://localhost:${port}`);
+});
