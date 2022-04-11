c497f3d0981f15c11d8dc852be509066b7092a81
Muniox <muniox20@gmail.com>
2022-04-11 23:17:13 +0200

    Added Routes with put and delete
    repaired url-encode by changing place in code
    str.93

--- meadowlark.js
+++ meadowlark.js
@@ -24,32 +24,79 @@ app.disable("x-powered-by");
 //dodanie plików statycznych
 app.use(express.static(__dirname + "/public"));
 
+//dodanie obsługi formularzy
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //konfiguracja portu na którym nasłuchuje serwer
 const port = process.env.PORT || 3000;
 
+const tours = [
+  { id: 0, name: "Hood River", price: 99.99 },
+  { id: 1, name: "Oregon Coast", price: 149.95 },
+];
+
 //trasowanie
 app.get("/", handlers.home);
-
 app.get("/about", handlers.about);
-
 app.get("/headers", handlers.headers);
-
 app.post("/process-contact", (req, res) => {
-  console.log(
-    `otrzymano dane kontaktowe od ${req.body.name} <${req.body.email}>`
-  );
-  res.redirect(303, "10-thank-you");
+  try {
+    if (req.body.simulateError) throw new Error("Błąd zapisu kontaktu");
+    console.log(
+      `otrzymano dane kontaktowe od ${req.body.name} <${req.body.email}>`
+    );
+    res.format({
+      "text/html": () => res.redirect(303, "/thank-you"),
+      "application/json": () =>
+        res.json({
+          success: true,
+        }),
+    });
+  } catch (err) {
+    console.log(
+      `błąd przetwarzania kontaktu od ${req.body.name} <${req.body.email}>`
+    );
+    res.format({
+      "text/html": () => res.redirect(303, "/contact-error"),
+      "application/json": () =>
+        res.status(500).json({
+          error: "błąd zapisu informacji kontaktowych",
+        }),
+    });
+  }
 });
-// app.get("/greeting", (req, res) => {
-//   res.render("about", {
-//     message: "Witaj, szanowny programisto",
-//     style: req.query.style,
-//     // userid: req.cookies.userid,
-//     username: req.session.username,
-//   });
-// });
+app.get("/api/tours", (req, res) => {
+  const toursXml =
+    '<?xml version="1.0"?><tours>' +
+    tours
+      .map((p) => `<tour price="${p.price}" id="${p.id}">${p.name}</tour>`)
+      .join("") +
+    "</tours>";
+  const toursText = tours
+    .map((p) => `${p.id}: ${p.name} (${p.price})`)
+    .join("\n");
+  res.format({
+    "aplication/json": () => res.json(tours),
+    "application/xml": () => res.type("aplication/xml").send(toursXml),
+    "text/xml": () => res.type("text/xml").send(toursXml),
+    "text/plain": () => res.type("text/plain").send(toursText),
+  });
+});
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.put("/api/tour/:id", (req, res) => {
+  const p = tours.find((p) => p.id === parseInt(req.params.id));
+  if (!p) return res.status(404).json({ error: "Nie istnieje taka wycieczka" });
+  if (req.body.name) p.name = req.body.name;
+  if (req.body.price) p.price = req.body.price;
+  res.json({ success: true });
+});
+
+app.delete("/api/tour/:id", (req, res) => {
+  const idx = tours.findIndex((tour) => tour.id === parseInt(req.params.id));
+  if (idx < 0) return res.json({ error: "Nie istnieje taka wycieczka" });
+  tours.splice(idx, 1);
+  res.json({ success: true });
+});
 
 //app.use z bledami serwera zawsze ponizej trasowania!
 //Niestandardowa strona 404
